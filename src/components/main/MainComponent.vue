993cc6a40f7b3452b43fa3e76a8ac904bde72ef9
alemonacoo <ale.monaco@outlook.com>
2022-09-26 19:45:32 +0200

    adding comics

--- src/components/main/MainComponent.vue
+++ src/components/main/MainComponent.vue
@@ -1,19 +1,28 @@
 <template>
   <main>
-    <div class="wallpaper"></div>
-    <div class="comics-container"></div>
+    <div class="wallpaper">
+    </div>
+    <div class="comics-container">
+      <div v-for="card in comics" :key="card.series">
+      <CardComponent :card="card"/>
+      </div>
+    </div>
   </main>
 </template>
 
 <script>
+import CardComponent from '@/components/main/cardComponent.vue';
 
 export default {
     name: 'MainComponent',
+    components:{
+      CardComponent
+    },
     props:{
       comics: Array
     },  
     mounted() {
-    console.log(this.comics);
+    console.log(this.comics, 'ricevuto da main component');
   },
 
 }
@@ -22,12 +31,27 @@ export default {
 <style lang="scss" scoped>
 @import '@/components/main.scss';
 main{
-  @include my-width;
-  @include flex-row;
   align-items: center;
   min-height: 15vh;
   color: white;
-  background-color: black;
+  width: 100vw;
+  .wallpaper{
+    color: white;
+    height: 23vw;
+    background-image: url(@/assets/img/jumbotron.jpg);
+    background-size: cover;
+  }
+  .comics-container{
+    @include my-width;
+    @include flex-row;
+    flex-wrap: wrap;
+    min-height: 50vh;
+    background-color: rgb(28,28,28);
+  }
+    
+
+
+
 }
 
 
---
+++ src/components/main/cardComponent.vue
@@ -0,0 +1,33 @@
+<template>
+<div class="card">
+    <img :src="card.thumb" alt="">
+    {{card.series}}
+</div>
+</template>
+
+<script>
+export default {
+    name: 'CardComponent',
+    props:{
+        card: Object,
+    },
+    mounted(){
+        console.log(this.cards, 'ricevuto da cardcomponent');
+    }
+
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/components/main.scss';
+    .card{
+    @include flex-col;
+    padding: 20px;
+    img{
+        height: 170px;
+        width: 170px;
+        object-fit: cover;
+        object-position: top;
+    }
+    }
+</style>
