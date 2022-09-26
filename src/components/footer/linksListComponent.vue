d7342f7a42974596cefc5a90b1526458df080fc9
alemonacoo <ale.monaco@outlook.com>
2022-09-26 16:45:01 +0200

    middle footer component complete!

---
+++ src/components/footer/linksListComponent.vue
@@ -0,0 +1,46 @@
+<template>
+<div>
+    <ul>
+        <h3>Titolo lista</h3>
+        <li>Link</li>
+        <li>Link</li>
+        <li>Link</li>
+        <li>Link</li>
+        <li>Link</li>
+    </ul>
+
+</div>
+  
+</template>
+
+<script>
+export default {
+    name: 'linksListComponent'
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/components/main.scss';
+div{
+    @include flex-row;
+    width: calc(100%/3);
+    h3{
+        padding: 5px 0px;
+        font-size: 0.9rem;
+    }
+    ul{
+        padding: 5px;
+    @include flex-col;
+    list-style-type: none;
+        li{
+            color: grey;
+            font-size: 0.8rem;
+        }
+
+    }
+}
+
+
+
+
+</style>
--- src/components/footer/middleFooter.vue
+++ src/components/footer/middleFooter.vue
@@ -1,18 +1,59 @@
 <template>
-<div>Middle Footer</div>
+<footer>
+    <div>
+        <linksListComponent/>
+        <linksListComponent/>
+        <linksListComponent/>
+        <linksListComponent/>
+    </div>
+        <img src="@/assets/img/dc-logo-bg.png" alt="dc footer logo">
+    
+
+
+
+</footer>
 </template>
 
+
 <script>
+
+import linksListComponent from '@/components/footer/linksListComponent.vue'
+
 export default {
-    name: 'MiddleFooter'
+    name: 'MiddleFooter',
+    components:{ linksListComponent}
 
 }
 </script>
 
 <style lang="scss" scoped>
 @import '@/components/main.scss';
-div{
+footer{
     @include my-width;
+    @include flex-row;
+    max-height: 46vh;
+    width: 100vw;
+    background-image: url(@/assets/img/footer-bg.jpg);
+    background-size: cover;
+    justify-content: space-between;
+    overflow-y: hidden;
+    div{
+        @include flex-row;
+        flex-grow: 1;
+        flex-wrap: wrap;
+        color: white;
+        ul{
+            @include flex-col;
+            list-style-type: none;
+        }
+
+    }
+    img{
+    width: 50%;
+    height: 100%;
+    transform: translateY(-70px);
+    flex-grow: 0;
+    }
 }
 
 
